6d971afda28804125836fa3e53b08072b4c0a767
Hyunyung Chae <bubsakk@gmail.com>
2019-12-10 14:30:26 +0900

    chat.js port change

--- chat.js
+++ chat.js
@@ -41,9 +41,12 @@ io.on('connection', function(socket){ //3
     io.emit('receive message', msg);
   });
 });
+var server_port = process.env.YOUR_PORT || process.env.PORT || 8000;
 
-http.listen(process.env.port || 8000, function(){ //4
-  console.log('server on! listening '+process.env.port+' || 8000');
+//http.listen(process.env.port || 8000, function(){ //4
+http.listen(server_port, function(){ //4
+	console.log('server on! listening '+server_port);	
+//  console.log('server on! listening '+process.env.port+' || 8000');
 });
 function saveData (Account, msg){
 // array 1000 개 만들어서 이전 1000개의 대화명/대화 저장해서 누구 접속할때마다 띄워줄것
