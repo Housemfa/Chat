ce76ae93174378ab00468e5b4124f9dfeabd1a3e
Hyunyung Chae <bubsakk@gmail.com>
2019-12-10 14:23:54 +0900

    port changes

--- chat.js
+++ chat.js
@@ -42,8 +42,8 @@ io.on('connection', function(socket){ //3
   });
 });
 
-http.listen(8000, function(){ //4
-  console.log('server on! listening 8000');
+http.listen(process.env.port || 8000, function(){ //4
+  console.log('server on! listening '+process.env.port+' || 8000');
 });
 function saveData (Account, msg){
 // array 1000 개 만들어서 이전 1000개의 대화명/대화 저장해서 누구 접속할때마다 띄워줄것
